e4f39f6834aadef947caf2d8154b8f41ebb71caa
Ville Koskela <vkoskela@gmail.com>
2021-09-27 20:51:37 +0300

    4.14 done

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -20,4 +20,39 @@ blogsRouter.post('/', async (request, response, next) => {
   }
 })
 
+blogsRouter.delete('/:id', async (request, response, next) => {
+  const id = request.params.id
+  try {
+    const res = await Blog.findByIdAndDelete(id)
+    if (res === null) {
+      // 404
+      response.status(404).end()
+    }
+    response.status(204).end()
+  } catch (error) {
+    next(error)
+  }
+})
+
+blogsRouter.put('/:id', async (request, response, next) => {
+  const id = request.params.id
+  try {
+    const res = await Blog.findOneAndUpdate({ _id: id },
+      {
+        title: request.body.title,
+        author: request.body.author,
+        url: request.body.url,
+        likes: request.body.likes
+      }, { runValidators: true })
+    if (res) {
+      const updatedBlog = await Blog.findById({ _id: id })
+      response.json(updatedBlog)
+    }
+    else
+      response.status(404).end()
+  } catch (error) {
+    next(error)
+  }
+})
+
 module.exports = blogsRouter
--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -12,55 +12,105 @@ const blogObj = {
   url: 'http://testblog.example.com'
 }
 
-beforeEach(async () => {
-  await Blog.deleteMany({})
-  await Blog.insertMany(helper.testBlogs)
-})
-
 describe('blogs api', () => {
-  test('blogs are returned as json', async () => {
-    await api.get('/api/blogs').expect(200).expect('Content-Type', /application\/json/)
+
+  beforeEach(async () => {
+    await Blog.deleteMany({})
+    await Blog.insertMany(helper.testBlogs)
   })
 
-  test('blogs have id instead of _id', async () => {
-    const response = await api.get('/api/blogs')
-    response.body.map(blog => {
-      expect(blog).toHaveProperty('id')
-      expect(blog).not.toHaveProperty('_id')
+  describe('GET tests and JSON format', () => {
+    test('blogs are returned as json', async () => {
+      await api.get('/api/blogs').expect(200).expect('Content-Type', /application\/json/)
+    })
+
+    test('blogs have id instead of _id', async () => {
+      const response = await api.get('/api/blogs')
+      response.body.map(blog => {
+        expect(blog).toHaveProperty('id')
+        expect(blog).not.toHaveProperty('_id')
+      })
+    })
+
+    test('all blogs are returned', async () => {
+      const response = await api.get('/api/blogs')
+      expect(response.body).toHaveLength(helper.testBlogs.length)
+    })
+  })
+  describe('POST new blogs', () => {
+    test('post blog is working', async () => {
+      await api.post('/api/blogs').send(blogObj).expect(201).expect('Content-Type', /application\/json/)
+      const response = await api.get('/api/blogs')
+      expect(response.body).toHaveLength(helper.testBlogs.length + 1)
+      expect(response.body.map(blog => blog.author)).toContain(blogObj.author)
+      expect(response.body.map(blog => blog.title)).toContain(blogObj.title)
+    })
+
+    test('make sure likes default to 0', async () => {
+      // make sure our test object doesn't have 'likes' before posting to api
+      expect(blogObj).not.toHaveProperty('likes')
+      const res = await api.post('/api/blogs').send(blogObj)
+      expect(res.body).toHaveProperty('likes')
+      expect(res.body.likes).toBe(0)
+      expect(res.body.likes).not.toBe(1)
+    })
+
+    test('make sure api sends 400 if mandatory properties are missing', async () => {
+      // missing url
+      await api.post('/api/blogs').send({ title: '123', author: 'Name' }).expect(400).expect('Content-Type', /application\/json/)
+      // missing title
+      await api.post('/api/blogs').send({ author: 'Name', url: 'http://foo.example.com' }).expect(400).expect('Content-Type', /application\/json/)
+      // missing everything
+      await api.post('/api/blogs').send({}).expect(400).expect('Content-Type', /application\/json/)
     })
   })
 
-  test('all blogs are returned', async () => {
-    const response = await api.get('/api/blogs')
-    expect(response.body).toHaveLength(helper.testBlogs.length)
+  describe('DELETE blogs', () => {
+    test('allow deleting blog by id', async () => {
+      const blogsInDb = await helper.blogsInDB()
+      const res = await api.delete(`/api/blogs/${blogsInDb[0].id}`)
+      expect(res.status).toBe(204)
+      const allBlogs = await api.get('/api/blogs')
+      expect(allBlogs.body).toHaveLength(helper.testBlogs.length - 1)
+      expect(allBlogs.body.map(b => b.id)).not.toContain(blogsInDb[0].id)
+    })
+    test('delete with id not found in db returns 404', async () => {
+      const nonExistingId = await helper.nonExistingId()
+      const res = await api.delete(`/api/blogs/${nonExistingId}`)
+      expect(res.status).toBe(404)
+      const allBlogs = await api.get('/api/blogs')
+      expect(allBlogs.body).toHaveLength(helper.testBlogs.length)
+    })
   })
 
-  test('post blog is working', async () => {
-    await api.post('/api/blogs').send(blogObj).expect(201).expect('Content-Type', /application\/json/)
-    const response = await api.get('/api/blogs')
-    expect(response.body).toHaveLength(helper.testBlogs.length + 1)
-    expect(response.body.map(blog => blog.author)).toContain(blogObj.author)
-    expect(response.body.map(blog => blog.title)).toContain(blogObj.title)
-  })
-
-  test('make sure likes default to 0', async () => {
-    // make sure our test object doesn't have 'likes' before posting to api
-    expect(blogObj).not.toHaveProperty('likes')
-    const res = await api.post('/api/blogs').send(blogObj)
-    expect(res.body).toHaveProperty('likes')
-    expect(res.body.likes).toBe(0)
-    expect(res.body.likes).not.toBe(1)
-  })
-
-  test('make sure api sends 400 if mandatory properties are missing', async () => {
-    // missing url
-    await api.post('/api/blogs').send({ title: '123', author: 'Name' }).expect(400).expect('Content-Type', /application\/json/)
-    // missing title
-    await api.post('/api/blogs').send({ author: 'Name', url: 'http://foo.example.com' }).expect(400).expect('Content-Type', /application\/json/)
-    // missing everything
-    await api.post('/api/blogs').send({}).expect(400).expect('Content-Type', /application\/json/)
+  describe('PUT blogs', () => {
+    test('allow adding a like to a blog by id', async () => {
+      const blogsInDb = await helper.blogsInDB()
+      const id = blogsInDb[0].id
+      const likes = blogsInDb[0].likes
+      const updatedBlog = { ...blogsInDb[0], likes: likes + 1 }
+      const res = await api.put(`/api/blogs/${id}`).send(updatedBlog)
+      expect(res.status).toBe(200)
+      expect(res.body).toEqual(updatedBlog)
+    })
+    test('allow editing title, author and url of blog', async () => {
+      const blogsInDb = await helper.blogsInDB()
+      const updatedBlog = { ...blogsInDb[0], title: 'foo', author: 'bar', url: 'http://www.blog.example.com' }
+      const res = await api.put(`/api/blogs/${updatedBlog.id}`).send(updatedBlog)
+      expect(res.status).toBe(200)
+      expect(res.body).not.toEqual(blogsInDb[0])
+      expect(res.body).toEqual(updatedBlog)
+    })
+    test('editing with non-existing id gives 404', async () => {
+      const nonExistingId = await helper.nonExistingId()
+      const blog = { id: nonExistingId, title: 'foo', author: 'bar', url: 'http://foo.example.com', likes: 100 }
+      const res = await api.put(`/api/blogs/${nonExistingId}`).send(blog)
+      expect(res.status).toBe(404)
+      expect(res.body).not.toEqual(blog)
+    })
   })
 })
+
 
 afterAll(() => {
   mongoose.connection.close()
